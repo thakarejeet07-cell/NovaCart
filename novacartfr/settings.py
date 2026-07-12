73562a35c6070a7e0291de383726692bcaf8fbae
Jeet Thakare <thakarejeet07@gmail.com>
2026-07-13 00:27:51 +0530

    Add Product model with image field, register in admin

--- novacartfr/settings.py
+++ novacartfr/settings.py
@@ -120,3 +120,6 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
--- store/models.py
+++ store/models.py
@@ -6,3 +6,15 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)   
+    price = models.DecimalField(max_digits=7, decimal_places=2)
+    description = models.TextField()
+    image = models.ImageField(upload_to='products/')
+    stock = models.IntegerField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return self.name
