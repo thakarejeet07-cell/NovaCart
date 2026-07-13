a0abd5ac5bd039f5404106af010e4a048ab2ef6c
Jeet Thakare <thakarejeet07@gmail.com>
2026-07-14 00:32:26 +0530

    Display products on homepage, add sample products via admin

--- store/views.py
+++ store/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
 def home(request):
-    return render(request,'web/index.html')
+    products = Product.objects.all()
+    return render(request,'web/index.html',{'products': products})
     
